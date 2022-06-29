287afe5f5b535f52c6aef7467bb0da766feeb976
Nick <morphias92@gmail.com>
2022-06-28 21:56:55 -0700

    initial setup stuff

---
+++ star-wars-app/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Planets />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Planets from "@/components/Planets.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    Planets,
+  },
+};
+</script>
